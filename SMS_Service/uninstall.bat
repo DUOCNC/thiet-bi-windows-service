54754d0104de715e36d17ac2c592976374e4e8c6
tvctk5 <trai.it.utehy@gmail.com>
2019-03-03 10:32:04 +0700

    Update ReleaseVesion: 20190228

--- SMS_Service/uninstall.bat
+++ SMS_Service/uninstall.bat
@@ -4,8 +4,8 @@ REM The following directory is for .NET 4.0
 set DOTNETFX2=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319
 set PATH=%PATH%;%DOTNETFX2%
 
-echo Uninstalling SMS_Service.exe...
+echo Uninstalling C:\Services\SMS\SMS_Service.exe...
 echo ---------------------------------------------------
-InstallUtil -u SMS_Service.exe
+InstallUtil -u C:\Services\SMS\SMS_Service.exe
 echo ---------------------------------------------------
 echo Done.
